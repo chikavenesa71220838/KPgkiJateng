aad2a830dd746ac55b4b3ee62a73295836dcba87
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2025-09-19 17:57:22 +0700

    configure backend awal

---
+++ mobile-gereja/app/index.tsx
@@ -0,0 +1,6 @@
+import { Redirect } from 'expo-router';
+
+export default function Index() {
+  // Mengarahkan langsung ke halaman jadwal ibadah yang ada di dalam grup (tabs)
+  return <Redirect href="/jadwalIbadah" />;
+}
