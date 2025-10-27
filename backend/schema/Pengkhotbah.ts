67866e70a4ed72ea0d243c67a102228f6701ea19
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2025-10-27 13:14:46 +0700

    update ke backend pendeta

--- backend/schema/Pengkhotbah.ts
+++ backend/schema/Pengkhotbah.ts
@@ -1,5 +1,5 @@
 import { list } from "@keystone-6/core";
-import { text, relationship } from "@keystone-6/core/fields";
+import { text, relationship, image } from "@keystone-6/core/fields";
 
 const allowAll = {
   operation: {
@@ -13,8 +13,23 @@ const allowAll = {
 export const Pengkhotbah = list({
   access: allowAll,
   fields: {
+    gambarProfil: image({
+      storage: "local_images",
+      hooks: {
+        validateInput: async ({ resolvedData, addValidationError }) => {
+          const file = resolvedData.gambarProfil;
+          if (
+            file &&
+            file.mimetype !== "image/jpeg" &&
+            file.mimetype !== "image/jpg"
+          ) {
+            addValidationError("Hanya file JPEG atau JPG yang diperbolehkan.");
+          }
+        },
+      },
+    }),
     nama: text({ validation: { isRequired: true } }),
-    // kontak: text(),
+    kontak: text(),
     detailIbadah: relationship({ ref: "DetailIbadah.pengkhotbah", many: true }),
   },
   ui: { labelField: "nama" },
