df90a4fd9c891e68fd9e464e57d2e4c2eb68286c
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2025-10-28 16:53:19 +0700

    isi database

--- backend/.keystone/admin/.next/server/middleware-build-manifest.js
+++ backend/.keystone/admin/.next/server/middleware-build-manifest.js
@@ -9,11 +9,6 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -24,15 +19,20 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/wartas": [
+    "/gerejas": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/wartas.js"
+      "static/chunks/pages/gerejas.js"
     ],
-    "/wartas/[id]": [
+    "/gerejas/[id]": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/wartas/[id].js"
+      "static/chunks/pages/gerejas/[id].js"
+    ],
+    "/gerejas/create": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/gerejas/create.js"
     ]
   },
   "ampFirstPages": []
--- backend/.keystone/admin/.next/static/chunks/webpack.js
+++ backend/.keystone/admin/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6aac6c5273e074c8"; }
+/******/ 		__webpack_require__.h = function() { return "14f2924bdf23dc73"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
