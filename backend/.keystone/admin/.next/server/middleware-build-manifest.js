bc4acbb87700ce396a32ac143cb9cf599e44f3f6
71220838 Chika Venesa <159112644+chikavenesa71220838@users.noreply.github.com>
2026-02-19 13:49:49 +0700

    pus

--- backend/.keystone/admin/.next/server/middleware-build-manifest.js
+++ backend/.keystone/admin/.next/server/middleware-build-manifest.js
@@ -9,11 +9,6 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -24,10 +19,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/detail-ibadahs": [
+    "/users": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/detail-ibadahs.js"
+      "static/chunks/pages/users.js"
     ]
   },
   "ampFirstPages": []
