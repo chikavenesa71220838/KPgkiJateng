a7a52c40de066dfb44c10b6cf21b047bb4e6381b
71220935_Dwiyan Bagus Prakosa <71220935@students.ukdw.ac.id>
2026-04-11 12:51:08 +0700

    remove .keystone from tracking

--- backend/.keystone/admin/pages/init.js
+++
@@ -1,5 +0,0 @@
-import { getInitPage } from '@keystone-6/auth/pages/InitPage';
-
-const fieldPaths = ["name","email","password"];
-
-export default getInitPage({"listKey":"Admin","fieldPaths":["name","email","password"],"enableWelcome":true});
--- backend/.keystone/admin/pages/signin.js
+++
@@ -1,3 +0,0 @@
-import { getSigninPage } from '@keystone-6/auth/pages/SigninPage'
-
-export default getSigninPage({"identityField":"email","secretField":"password","mutationName":"authenticateAdminWithPassword","successTypename":"AdminAuthenticationWithPasswordSuccess","failureTypename":"AdminAuthenticationWithPasswordFailure"});
